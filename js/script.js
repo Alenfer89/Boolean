76e74b0082fa00e0eb423400ffc6881a83699711
Alenfer89 <alberioalex@gmail.com>
2022-02-21 18:21:27 +0100

    random-things

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const firstName = prompt ("Nome?");
+document.getElementById("first-name").innerHTML = firstName;
+
+const familyName = prompt ("Cognome?");
+document.getElementById("last-name").innerHTML = familyName;
+
+const favouriteColor = prompt ("Colore preferito?");
+document.getElementById("color").innerHTML = favouriteColor;
+
