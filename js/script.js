5adbe5520df175e16de6dd7338ef37ba3a06f875
Alenfer89 <alberioalex@gmail.com>
2022-02-21 18:29:56 +0100

    some-more-random-things

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const firstName = prompt ("Nome?");
-document.getElementById("first-name").innerHTML = firstName;
+console.log(firstName)
+// document.getElementById("first-name").innerHTML = firstName;
 
 const familyName = prompt ("Cognome?");
-document.getElementById("last-name").innerHTML = familyName;
+console.log(familyName)
+// document.getElementById("last-name").innerHTML = familyName;
 
 let favouriteColor = prompt ("Colore preferito?");
-document.getElementById("color").innerHTML = favouriteColor;
+console.log(favouriteColor)
+// document.getElementById("color").innerHTML = favouriteColor;
 
 let mySuperSafePassword = firstName + familyName + favouriteColor + 21;
 document.getElementById("password-result").innerHTML = mySuperSafePassword;
+console.log(mySuperSafePassword)
