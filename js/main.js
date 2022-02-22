3c01c128a91c359c92d3b46ceb23ce9221b989d3
Alenfer89 <alberioalex@gmail.com>
2022-02-22 18:00:27 +0100

    random

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+const customerAge = parseInt(prompt("eta?"));
+console.log(customerAge)
+const customerDistance = parseInt(prompt("kilometri?"))
+console.log(customerDistance)
 
+if (isNaN(customerAge) || isNaN(customerDistance)){
+    alert("inserire un valore numerico")
+}
+
+const price = customerDistance * 0.21;
+console.log(price)
+
+let finalPrice;
+
+if (customerAge < 18){
+    finalPrice = price - price * 0.2;
+    console.log(finalPrice);
+} else if(customerAge > 65){
+    finalPrice = price - price * 0.4;
+    console.log(finalPrice);
+} else {
+    finalPrice = price;
+    console.log(finalPrice);
+}
 
 
