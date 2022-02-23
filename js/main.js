af5bf4e49bc516537a6cab0b435404237710eae0
Alenfer89 <alberioalex@gmail.com>
2022-02-23 17:18:47 +0100

    data-cor-ver

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-let papName = document.querySelector("#pap-name").innerHTML;
+let papName = document.querySelector("#pap-name").value;
 console.log(papName);
 
-let papDistance = parseInt(document.querySelector("#pap-distance").innerHTML);
+let papDistance = parseInt(document.querySelector("#pap-distance").value);
 console.log(papDistance);
 
 let papAge = document.querySelector("#pap-age").value;
 console.log(papAge);
 
 const basicCost = 0.27;
-const price = basicConst * papDistance;
+const price = basicCost * papDistance;
