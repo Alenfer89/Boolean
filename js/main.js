d122f9f647f41aea2a4c5db58d75d26340246de1
Alenfer89 <alberioalex@gmail.com>
2022-02-23 17:32:11 +0100

    not-working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-let papName = document.querySelector("#pap-name").value;
-console.log(papName);
+
 
 let papDistance = parseInt(document.querySelector("#pap-distance").value);
 console.log(papDistance);
@@ -9,3 +8,17 @@ console.log(papAge);
 
 const basicCost = 0.27;
 const price = basicCost * papDistance;
+
+document.querySelector("#generator").addEventListener('click', function(){
+    let ticketName = document.createElement("p");
+
+    const papName = document.querySelector("#pap-name").value;
+    console.log(papName);
+
+    ticketName.innerHTML = papName;
+    console.log(ticketName);
+
+    document.querySelector("#ticket-name").append(ticketName);
+
+
+})
