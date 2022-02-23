439017fa4798b5abfc806bc17e5d7fee061acff6
Alenfer89 <alberioalex@gmail.com>
2022-02-23 17:14:24 +0100

    some-random-data

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let papName = document.querySelector("#pap-name").innerHTML;
+console.log(papName);
+
+let papDistance = parseInt(document.querySelector("#pap-distance").innerHTML);
+console.log(papDistance);
+
+let papAge = document.querySelector("#pap-age").value;
+console.log(papAge);
+
+const basicCost = 0.27;
+const price = basicConst * papDistance;
