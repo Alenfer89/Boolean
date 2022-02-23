4b34975fe8ab2ff9298aff6668cfa039e38f0ba1
Alenfer89 <alberioalex@gmail.com>
2022-02-23 19:20:18 +0100

    nvm

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-
-    
-
-let papDistance = parseInt(document.querySelector("#pap-distance").value);
-console.log(papDistance);
-
-
-// if (isNaN(papDistance)){
-//     alert("Per favore inserisci un nuimero")
-// }
-
-// let papAge = document.querySelector("#pap-age").value;
-// console.log(papAge);
-
-const basicCost = 0.27;
-const price = basicCost * parseInt(papDistance);
-
 document.querySelector("#generator").addEventListener('click', function(){
     let ticketName = document.createElement("p");
     const papName = document.querySelector("#pap-name").value;
@@ -24,15 +7,22 @@ document.querySelector("#generator").addEventListener('click', function(){
     document.querySelector("#ticket-name").append(ticketName);
 
     let ticketDistance = document.createElement("li");
+    const papDistance = parseInt(document.querySelector("#pap-distance").value);
+    console.log(papDistance);
     ticketDistance.innerHTML = papDistance;
     console.log(ticketDistance);
     document.querySelector("#customer").append(ticketDistance);
+
+    const basicCost = 0.27;
+    const price = basicCost * parseInt(papDistance) ;
 
     let ticketPrice = document.createElement("li");
     ticketPrice.innerHTML = price;
     console.log(ticketPrice);
     document.querySelector("#customer").append(ticketDistance);
+
 })
+
 
 document.querySelector("#get-back").addEventListener('click', function(){
     document.getElementById("pap-name").value="";
