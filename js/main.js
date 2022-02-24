2665f0a2b3419f3a289cbbcb29c4056ea5064032
Alenfer89 <alberioalex@gmail.com>
2022-02-24 19:32:04 +0100

    zero-removed-from-assignment

--- js/main.js
+++ js/main.js
@@ -10,11 +10,11 @@
 
 for(let i = 0; i <= 100; i = i + 1){
     
-    if ( (i % 3 == 0) && (i % 5 ==0)){
+    if ( (i % 3 == 0) && (i % 5 ==0) && (i !== 0)){
         console.log('FizzBuzz');
-    } else if(i % 3 == 0){
+    } else if((i % 3 == 0) && (i !== 0)){
         console.log('Fizz');
-    } else if(i % 5 ==0){
+    } else if((i % 5 ==0) && (i !== 0)){
         console.log('Buzz');
     } else{
         console.log(i)
