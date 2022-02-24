a40eb861142a41568e39fcc119e64052cb48434f
Alenfer89 <alberioalex@gmail.com>
2022-02-24 15:30:19 +0100

    retry

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+
+
+// Il programma dovrà mostrare una form (a piacere) da compilare con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.27 € al km)
+// va applicato uno sconto del 17% per i minorenni
+// va applicato uno sconto del 33% per gli over 65.
+// L'output del prezzo finale va visualizzato con massimo due decimali, per indicare centesimi sul prezzo.
+
+let papName = document.querySelector("#pap-name");
+let papKm = document.querySelector("#pap-distance");
+let papAge = document.querySelector("#pap-age");
+const basicCost = 0.27;
+
 document.querySelector("#generator").addEventListener('click', function(){
     let ticketName = document.createElement("p");
     const papName = document.querySelector("#pap-name").value;
@@ -13,7 +27,7 @@ document.querySelector("#generator").addEventListener('click', function(){
     console.log(ticketDistance);
     document.querySelector("#customer").append(ticketDistance);
 
-    const basicCost = 0.27;
+    
     const price = basicCost * parseInt(papDistance) ;
 
     let ticketPrice = document.createElement("li");
