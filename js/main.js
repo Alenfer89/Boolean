fbc93fd8fe537ac1721c5e5613924f729a6db96a
Alenfer89 <alberioalex@gmail.com>
2022-02-25 15:49:49 +0100

    added-some-style-and-js-comments+data

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Consigli generali
+// Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare attraverso le nostre istruzioni
+// si, ma noi cosa vogliamo fare?
+// torniamo a scrivere in italiano
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// usiamo i diagrammini e i discorsi filosofici di oggi come base per costruire un nostro sistema di implementazione autonomo
+
+const userEmail = document.getElementById('user-email');
+const registeredEmailList = ['pera', 'mela', 'banana','orange', 'melanzana', 'pomodoros', 'ciliegie', 'ananas'] ;
+console.log(userEmail);
+console.log(registeredEmailList);
