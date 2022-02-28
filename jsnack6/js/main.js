d745b2a34c827e71b7a6769a21d677c2f4d84ac9
Alenfer89 <alberioalex@gmail.com>
2022-02-28 16:58:14 +0100

    isnan-not-working-as-expected

--- jsnack6/js/main.js
+++ jsnack6/js/main.js
@@ -6,10 +6,12 @@
 
 //quello che volevo fare era chiedere, tramite prompt, un numero di 7 cifre all'utente. poi tramite un if scartare tutti gli inserimenti scorretti  o procedere con la richiesta della consegna. Volevo, tramite ricerca su MDN andata per le lunghe, scomporre la stringa ricevuta col prompt, mettere tutto in un array e infine sommare le cifre dell'array
 
-const userNumber = parseInt(prompt('indicami un numero di 7 cifre!'));
+const userNumber = prompt('indicami un numero di 7 cifre!');
+console.log(userNumber)
+console.log(userNumber.length)
 
-if (userNumber.length !== 7){
-
+if ((userNumber.length !== 7) || (isNaN(parseInt(userNumber))) ){
+  alert('inserisci 7 caratteri numerici')
 } else{
   userNumber
 }
