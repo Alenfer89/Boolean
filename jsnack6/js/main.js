66388e1dcb16a32ed08f946eca05f8d5eec04cc6
Alenfer89 <alberioalex@gmail.com>
2022-02-28 15:34:00 +0100

    jsnack6-comments-added

--- jsnack6/js/main.js
+++ jsnack6/js/main.js
@@ -4,10 +4,11 @@
  *
  */
 
+//quello che volevo fare era chiedere, tramite prompt, un numero di 7 cifre all'utente. poi tramite un if scartare tutti gli inserimenti scorretti  o procedere con la richiesta della consegna. Volevo, tramite ricerca su MDN andata per le lunghe, scomporre la stringa ricevuta col prompt, mettere tutto in un array e infine sommare le cifre dell'array
 
 const userNumber = parseInt(prompt('indicami un numero di 7 cifre!'));
 
-if (userNumber.length == 7){
+if (userNumber.length !== 7){
 
 } else{
   userNumber
