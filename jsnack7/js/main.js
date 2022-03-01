8434a7d7262d923f9c6a9fe73446ffd4bbf58992
Alenfer89 <alberioalex@gmail.com>
2022-03-01 15:11:58 +0100

    jsnascks-fallaci

---
+++ jsnack7/js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Creiamo un array con dieci numeri randomici
+ * calcoliamo la somma di tutti gli elementi
+ * e calcoliamo anche il valore medio
+ */
+
+
+
+let randomNumber= Math.floor(Math.random()*100);
+console.log(randomNumber);
+const myNumberList = [Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100)];
+console.log(myNumberList);
+for (let i = 0; i < 10; i++) {
+  let sum, 
+}
+let sum;
+let meadianValue;
