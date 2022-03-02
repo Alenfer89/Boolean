5d12cdaed5be11992903d0eef2c4d1a96be80ef4
Alenfer89 <alberioalex@gmail.com>
2022-03-02 15:18:17 +0100

    jsnacks-fallaci

---
+++ jsnack10/js/main.js
@@ -0,0 +1,13 @@
+/**
+    Fai inserire un numero, che chiameremo N, all'utente.
+    Genera N array, ognuno formato da 10 numeri casuali tra 4 e 4532.
+    Ogni volta che ne crei uno, stampalo a schermo.
+ */
+
+const richiesta = parseInt(prompt('inserisci un numero'));
+
+for (i = 0; i < richiesta.length; i++){
+  let x = i++;
+  const arrayNr=[];
+}
+console.log(arrayNr)
