56767f4149f87c97b3ea9bcc7ef3f6ed5699ee10
Alenfer89 <alberioalex@gmail.com>
2022-03-02 18:07:33 +0100

    jsnack10-revised

--- jsnack10/js/main.js
+++ jsnack10/js/main.js
@@ -3,11 +3,54 @@
     Genera N array, ognuno formato da 10 numeri casuali tra 4 e 4532.
     Ogni volta che ne crei uno, stampalo a schermo.
  */
+/** Ultimo snack di oggi versioni custom
+ *
+ * § BONUS 1
+ *
+    Fai inserire due numeri, che chiameremo N & M, all'utente.
+    Genera N array, ognuno formato da M numeri casuali tra 4 e 4532.
+    Ogni volta che ne crei uno, stampalo a schermo.
+
+ *
+ */
+  /**
+ *
+ * # BONUS 2
+ *
+    Fai inserire tre numeri, che chiameremo N & M & L, all'utente.
+    Genera N array, ognuno formato da M numeri casuali tra 4 e 4532 che sommati diano almeno L.
+    Ogni volta che ne crei uno, stampalo a schermo.
+
+ *
+ */
+
+  /**
+ *
+ * # BONUS 3
+ *
+    Fai inserire tre numeri, che chiameremo N & M & L, all'utente.
+    Genera N array, ognuno formato da M numeri casuali tra 4 e 4532 che sommati diano al più L.
+    Ogni volta che ne crei uno, stampalo a schermo.
+
+ *
+ */
 
 const richiesta = parseInt(prompt('inserisci un numero'));
-
-for (i = 0; i < richiesta.length; i++){
-  let x = i++;
-  const arrayNr=[];
+while (isNaN(richiesta)){
+  richiesta = parseInt(prompt('inserisci un numero'));
 }
-console.log(arrayNr)
+if (richiesta >= 100){
+  richiesta = 100;
+}
+
+const arrayList = [];
+
+for (i = 0; i < richiesta; i++){
+  const arrayNr=[];
+  while (arrayNr.length < 10){
+    arrayNr.push(Math.floor(Math.random()* 4528) +4)
+  }
+  console.log(arrayNr)
+  arrayList.push(arrayNr)
+}
+console.log(arrayList)
