a4d0c1b5d8f14733883792e2d1fc7abccecb2777
Alenfer89 <alberioalex@gmail.com>
2022-03-03 17:37:47 +0100

    double-flag-added

--- js/main.js
+++ js/main.js
@@ -21,22 +21,23 @@
 //   console.warn('diverse')
 // }
 
-//*creo le variabili per gli inpout dati dell'utente
+//.creo le variabili per gli inpout dati dell'utente
 let userChoice = document.getElementById('odd-or-even');
 let userNumber = document.getElementById('number');
 
-//*creo la funzione generatrice di numeri random
+//.creo la funzione generatrice di numeri random
 function randomNumberGenerator (Number){
   return Number = Math.floor(Math.random()* 5 + 1);
 }
 
-//*event listener su un bottone
+//.event listener su un bottone
 document.querySelector('button.btn.btn-primary').addEventListener('click', function(){
 
-  //*creo una variabile booleana per far partire il gioco dopo con un if
+  //.creo una variabile booleana per far partire il gioco dopo con un if
   let check = false;
+  let doubleCheck = false;
 
-  //*controllo che ivalori inseriti siano validi al click: prima "pari" o "dispari"
+  //.controllo che ivalori inseriti siano validi al click: prima "pari" o "dispari"
   if( (userChoice.value.toLowerCase() == 'pari') || (userChoice.value.toLowerCase() == 'dispari')){
     check = true;
     console.log('ok')
@@ -44,19 +45,20 @@ document.querySelector('button.btn.btn-primary').addEventListener('click', funct
     alert('inserisci valori corretti')
   }
 
-  //*poi "numero" o "nan"
+  //.poi "numero" o "nan"
   if( (isNaN(parseInt(userNumber.value))) ){
     alert('non è un numero')
   } else if ((userNumber.value < 1) || (userNumber.value > 5)){
     alert('inserisci un numero tra 1 e 5')
   } else{
-    check = true;
+    doubleCheck = true;
   }
 
-  //*il gioco:
-  if(check == false){
-    console.log('errore ti servono due flag')
+  //.il gioco:
+  if((check == false) || (doubleCheck == false)){
+    console.log('si riavvia il tutto')
   } else {
+    console.log('se c\'è stato un alert : ERRORE TI SERVONO DUE FLAG')
 
   }
 })
