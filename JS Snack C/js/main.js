ab2fbd4f8faa364e8564c863f0fd7e26adf15a59
Alenfer89 <alberioalex@gmail.com>
2022-03-11 15:57:42 +0100

    jsnack-C-fun

--- JS Snack C/js/main.js
+++ JS Snack C/js/main.js
@@ -14,10 +14,27 @@ const stringhe =['pippo', 'PLUTO', 'Paperino','asdfgaerr','IJHGH uYT VYg uhb', '
 //   betterStringhe.push(element)
 // }
 // console.log(betterStringhe)
-
+for (i = 0; i < randomIntFromInterval(5, 15); i++){
+  stringhe.push(makeId(randomIntFromInterval(3, 10)))
+}
 const betterStringhe = stringhe.map((element) =>{
   return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase()
 })
 console.table(stringhe)
 console.table(betterStringhe)
+
+function makeId(length) {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
 
