5364402e0585448e30635b490279b435f8030451
Alenfer89 <alberioalex@gmail.com>
2022-03-14 16:14:14 +0100

    propic-placeholder-test

--- main.js
+++ main.js
@@ -74,8 +74,43 @@ for (let i = 0; i < posts.length; i++){
 }
 
 function populatePosts (containerById, postText, postImage, postAuthor, authorProPic, likes){
-    document.getElementById(containerById).innerHTML += `
-    <div class="post">
+    if (authorProPic == null){
+        let placeholder = 'AX';
+        document.getElementById(containerById).innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        ${placeholder}                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${postAuthor}</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${postText}</div>
+            <div class="post__image">
+                <img src="${postImage}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `
+    } else {
+        document.getElementById(containerById).innerHTML += `
+        <div class="post">
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
@@ -106,4 +141,5 @@ function populatePosts (containerById, postText, postImage, postAuthor, authorPr
             </div>            
         </div>
         `
+    }
 }
