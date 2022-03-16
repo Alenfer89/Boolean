ef82789d710bc8e997a24f132d19d7c78e8637c3
Alenfer89 <alberioalex@gmail.com>
2022-03-16 15:58:05 +0100

    tests

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
+/**
+ * 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data, magari volendo sperimentare anche prendendola da un input.
+ */
+
+
 const app = new Vue (
    {
-      el: '#app'
+      el: '#app',
+      data : {
+         message : 'Ciao io sono un titolozzo',
+      }
    }
 )
+
+let scrivoaltro = 'apperò' ;
+console.log(scrivoaltro)
+console.log(app.message)
+app.message = scrivoaltro;
+//'Ciao io sono un titolozzo'
