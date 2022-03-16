37d6492d580b989590901d7ca836140476a19423
Alenfer89 <alberioalex@gmail.com>
2022-03-16 16:51:28 +0100

    random-funny-tests

--- js/main.js
+++ js/main.js
@@ -12,14 +12,36 @@ const app = new Vue (
    {
       el: '#app',
       data : {
-         message : 'Ciao io sono un titolozzo',
+         title : 'Ciao io sono un titolozzo',
          subtitle : '',
+         message : '',
+         image: '',
+         randomImg: '',
+      },
+      methods : {
+         randomIntFromInterval : function(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min)
+         },
+         randomPic : function (){
+            let random = this.randomIntFromInterval(1,999);
+            console.log(random)
+            return app.randomImg = `https://picsum.photos/id/${random}/200/300`;
+         },
+         messageViewer : function (){
+            if (this.randomImg == ''){
+               return 'immagine casuale';
+            } else {
+               return 'l\'immagine non esiste';
+            }
+         }
+         
       }
    }
 )
 
-let scrivoaltro = 'apperò' ;
-console.log(scrivoaltro)
-console.log(app.message)
-app.subtitle = scrivoaltro;
-//'Ciao io sono un titolozzo'
+let scrivoAltro = 'apperò' ;
+console.log(scrivoAltro);
+console.log(app.message);
+app.subtitle = scrivoAltro;
+let immagineRandom = 'https://picsum.photos/id/237/200/300';
+app.image = immagineRandom;
