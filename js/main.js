b6fadc449af6b352d7b53b25fc1dede5b7670d8b
Alenfer89 <alberioalex@gmail.com>
2022-03-17 17:19:30 +0100

    imgs-change

--- js/main.js
+++ js/main.js
@@ -7,27 +7,27 @@ const app = new Vue(
             scrollRight: true,
             carousel: [
                 {
-                    image: 'img/01.jpg',
+                    image: 'https://www.robintur.it/img/viaggi/svezia/image-thumb__5314__galleryCarousel/Svezia%20Lago~-~300w.pjpeg',
                     title: 'Svezia',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit',
                 },
                 {
-                    image: 'img/02.jpg',
+                    image: 'https://www.worldatlas.com/r/w1200/upload/db/48/05/shutterstock-411078559.jpg',
                     title: 'Svizzera',
                     text: 'Lorem ipsum',
                 },
                 {
-                    image: 'img/03.jpg',
+                    image: 'http://dhruvaoverseas.com/images/unk.jpg',
                     title: 'Gran Bretagna',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit',
                 },
                 {
-                    image: 'img/04.jpg',
+                    image: 'https://www.voglioviverecosi.com/wp-content/uploads/2017/10/TRAFERIRSI-IN-GERMANIA-1900x1080.jpg',
                     title: 'Germania',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
                 },
                 {
-                    image: 'img/05.jpg',
+                    image: 'https://content.skyscnr.com/m/69db90b2040d20ec/original/french-polynesia-bora-bora-over-water-bungalows-with-steps-into-amazing-lagoon-thinkstockphotos-140796261.jpg?resize=1200px:1200px&quality=100',
                     title: 'Paradise',
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
                 },
