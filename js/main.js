df0e1a221b42b25d4fb750c13fc64a813f0d3cd2
Alenfer89 <alberioalex@gmail.com>
2022-03-18 17:59:52 +0100

    boolean-not-working

--- js/main.js
+++ js/main.js
@@ -2,25 +2,35 @@ const app = new Vue(
     {
         el: '#app',
         data:{
-            seriesList:[],
+            seriesList:[
+                
+            ],
             singleSeries: '',
         },
         methods:{
-            addSeriesToList: function(title){
+            addSeriesToList: function(string){
                 if(this.singleSeries !== ''){
                     const series ={};
-                    series.title= title.trim();
+                    series.title= string.trim();
                     series.done = null,
                     this.seriesList.push(series)
                     this.singleSeries = '';
                 } else{
                     console.log('controllo spazi')
                 }
-                
             },
             removeSeriesToList: function(seriesIndex){
                 this.seriesList.splice(seriesIndex, 1)
             },
+            watchListModifier: function (object){
+                if(object.done = null){
+                    object.done = true;
+                    console.log(true)
+                } else if (object.done = true){
+                    object.done = false;
+                    console.log(false)
+                }
+            }
         }
     }
 )
