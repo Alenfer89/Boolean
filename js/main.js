44eda6822251aa866367e6d1ab533f9734e86383
Alenfer89 <alberioalex@gmail.com>
2022-03-24 17:41:40 +0100

    json-not-working

--- js/main.js
+++ js/main.js
@@ -9,12 +9,15 @@ const app = new Vue(
         }
     }
 );
+const emailArray=[];
 
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(function(response){
         console.log(response)
+        
         console.log(response.data)
         console.log(response.data.response)
+        console.log(JSON.parse(response.data))
 })
 for (let i = 0; i < 10; i++){
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
