47337aa336c5a293e578e839506f7215edc4e65c
Alenfer89 <alberioalex@gmail.com>
2022-03-24 17:14:21 +0100

    some-axios-tests

--- js/main.js
+++ js/main.js
@@ -16,4 +16,12 @@ axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         console.log(response.data)
         console.log(response.data.response)
 })
+for (let i = 0; i < 10; i++){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(function(response){
+        console.log(response.data.response)
+})
+}
+
+
 
