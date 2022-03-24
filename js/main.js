41859904e05e18b2764cbdd3e661a6195fa8f372
Alenfer89 <alberioalex@gmail.com>
2022-03-24 18:10:06 +0100

    array-created

--- js/main.js
+++ js/main.js
@@ -2,29 +2,40 @@ const app = new Vue(
     {
         el: '#app',
         data:{
-
+            emailArray: [],
         },
         methods:{
-
-        }
+        },
+        mounted(){
+            const self = this;
+            for (let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(function(response){
+                    console.log(response.data.response);
+                    self.emailArray.push(response.data.response); 
+            })
+            };
+            console.log(self.emailArray);
+        },
     }
 );
-const emailArray=[];
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(function(response){
-        console.log(response)
+// axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//     .then(function(response){
+//         console.log(response)
         
-        console.log(response.data)
-        console.log(response.data.response)
-        console.log(JSON.parse(response.data))
-})
-for (let i = 0; i < 10; i++){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(function(response){
-        console.log(response.data.response)
-})
-}
+//         console.log(response.data)
+//         console.log(response.data.response)
+//         //console.log(JSON.parse(response.data))
+// })
+
+
+// for (let i = 0; i < 10; i++){
+//     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//     .then(function(response){
+//         console.log(response.data.response)
+// })
+// }
 
 
 
