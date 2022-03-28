092d3b490e293379f4fc945cbe7df74c56e553f1
Alenfer89 <alberioalex@gmail.com>
2022-03-28 18:17:37 +0200

    footer-styled

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,48 +3,53 @@
 
         <div class="top-footer-container">
             <div class="top-footer">
-                <div class="lists-wrapper">
-                    <h4>TITLE</h4>
-                    <ul>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                    </ul>
-                    <h4>TITLE</h4>
-                    <ul>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                    </ul>
+                <div class="lists-container">
+                    <div class="list-wrapper">
+                        <h4>TITLE</h4>
+                        <ul>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                        </ul>
+                        <h4>TITLE</h4>
+                        <ul>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                        </ul>
+                    </div>
+                    <div class="list-wrapper">
+                        <h4>TITLE</h4>
+                        <ul>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                        </ul>
+                    </div>
+                    <div class="list-wrapper">
+                        <h4>TITLE</h4>
+                        <ul>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                            <li>ITEM</li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="lists-wrapper">
-                    <h4>TITLE</h4>
-                    <ul>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                    </ul>
-                </div>
-                <div class="lists-wrapper">
-                    <h4>TITLE</h4>
-                    <ul>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                        <li>ITEM</li>
-                    </ul>
+                
+                <div id="logo-background">
                 </div>
             </div>
         </div>
@@ -52,10 +57,10 @@
         <div class="bop-footer-container">
             <div class="bot-footer">
                 <div>
-                    <button>SIGN-UP NOW</button>
+                    <button>SIGN-UP NOW!</button>
                 </div>
                 <div class="socials-container">
-                    <div>FOLLOW US</div>
+                    <h4>FOLLOW US</h4>
                     <div>
                         <img src="../assets/img/footer-facebook.png" alt="">
                     </div>
@@ -92,16 +97,23 @@ export default {
         color: rgba(255, 255, 255, 0.5);
         div.top-footer-container{
         background-image: url('../assets/img/footer-bg.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
             div.top-footer{
                 width: 80%;
                 margin: 0 auto;
-                padding: 2rem 0;
                 display: flex;
-                position: relative;
                 flex-direction: row;
                 flex-wrap: nowrap;
-                justify-content: start;
-                div.lists-wrapper{
+                justify-content: space-between;
+                div.lists-container{    
+                    padding: 2rem 0;
+                    display: flex;
+                    flex-direction: row;
+                    flex-wrap: nowrap;
+                    justify-content: start;
+                }
+                div.list-wrapper{
                     margin-right: 1.6rem;
                 }
                 h4{
@@ -113,6 +125,14 @@ export default {
                     li{
                         font-size: .7rem;
                     }
+                }
+                div#logo-background{
+                    width: 40%;
+                    background-image: url('../assets/img/dc-logo-bg.png');
+                    background-position: -50%;
+                    background-size: 100%;
+                    background-repeat: no-repeat;
+                    
                 }
             }
         }
@@ -128,16 +148,20 @@ export default {
                 align-items: center;
                 button{
                     background-color: transparent;
+                    font-weight: 700;
                     color: white;
                     padding: 0.8rem;
                     border: 2px solid $productMainColor;
+                }
+                h4{
+                    color: $productMainColor;
                 }
                 div.socials-container{
                     display: flex;
                     flex-direction: row;
                     align-items: center;
                     img{
-                        margin: 0 1rem;
+                        margin-left: 2rem;
                     }
                 }
             }
