0c51aa41080b3deb200db0474b4101fc7ece5568
Alenfer89 <alberioalex@gmail.com>
2022-03-30 15:23:05 +0200

    some-structure-added-and-bootstrap-linked-via-scss

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1 class="text-danger">header</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'IndexHeader'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <h2>maain</h2>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'IndexMain'
+}
+</script>
+
+<style>
+
+</style>
