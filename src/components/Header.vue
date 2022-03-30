e779901eb65799f5e4f9c53f37bdf2b8b01b9a3e
Alenfer89 <alberioalex@gmail.com>
2022-03-30 15:56:22 +0200

    header-completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
     <header>
-        <h1 class="text-danger">header</h1>
+        <div class="img-wrapper p-3 ms-3">
+            <a href="https://www.spotify.com/it/">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/71/Spotify.png" alt="Spotify Logo" class="img-fluid">
+            </a>
+        </div>
     </header>
 </template>
 
@@ -11,5 +15,12 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+header{
+    background-color: rgb(46, 58, 70);
+
+    .img-wrapper{
+        width: 100px;
+    }
+}
 
 </style>
