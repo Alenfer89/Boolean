c034ebcf988ab709b0f5653911de173d30396190
Alenfer89 <alberioalex@gmail.com>
2022-04-01 15:20:43 +0200

    basic-header-structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
-    <header>
-        <h1>HEADER</h1>
+    <header class="d-flex justify-content-between align-items-center p-4">
+        <h1 class="text-danger text-uppercase">boolflix</h1>
+        <nav>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </nav>
     </header>
 </template>
 
