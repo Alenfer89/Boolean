417c13e9aae6bc4c2d00b0197ce8e67ff8c383f8
Alenfer89 <alberioalex@gmail.com>
2022-04-01 16:06:06 +0200

    nifos-showing-with-static-research

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,26 @@
 <template>
     <main class="text-center">
-        <div class="ax-film-wrapper">
-            <h1>titolo</h1>
-            <h2>titolo originale</h2>
-            <h3>lingua</h3>
-            <h4>voto</h4>
+        <div class="ax-movies-wrapper">
+            <Movie
+            v-for='(element, index) in moviesList'
+            :key='index'
+            :movie='element' />
         </div>
-        
-
     </main>
 </template>
 
 <script>
 import axios from 'axios';
+import Movie from './Movie.vue'
 
 export default {
     name: 'IndexMainContainer',
+    components: {
+        Movie,
+    },
     data: function (){
         return{
-
+            moviesList:[],
         }
     },
     methods:{
@@ -32,6 +34,7 @@ export default {
             .get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro')
             .then((result) =>{
                 console.log(result.data.results)
+                this.moviesList = result.data.results;
             })
             .catch((error) =>{
                 console.warn(error)
---
+++ src/components/Movie.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="ax-movie-card">
+        <h1> {{ movie.title }} </h1>
+        <h2> {{ movie['original_title'] }} </h2>
+        <h3> {{ movie['original_language'] }} </h3>
+        <h4> {{ movie['vote_average'] }} </h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'singleMovieCard',
+    props:['movie']
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
