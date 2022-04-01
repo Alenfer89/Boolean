dc874aa2aa754dd4a5a57ff58abcaad96c716719
Alenfer89 <alberioalex@gmail.com>
2022-04-01 15:28:50 +0200

    ajax-api-import

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,8 +5,31 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'IndexMainContainer',
+    data: function (){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    computed:{
+
+    },
+    created: function(){
+        axios
+            .get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro')
+            .then((result) =>{
+                console.log(result)
+            })
+            .catch((error) =>{
+                console.warn(error)
+            });
+    }
 }
 </script>
 
