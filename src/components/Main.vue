9ed8ed32b13569fdfcbdda5c79ae04382101993d
Alenfer89 <alberioalex@gmail.com>
2022-04-01 15:38:28 +0200

    main-contenet-basic-structure

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,13 @@
 <template>
-    <main>
-        <h1>MAIN</h1>
+    <main class="text-center">
+        <div class="ax-film-wrapper">
+            <h1>titolo</h1>
+            <h2>titolo originale</h2>
+            <h3>lingua</h3>
+            <h4>voto</h4>
+        </div>
+        
+
     </main>
 </template>
 
@@ -24,7 +31,7 @@ export default {
         axios
             .get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro')
             .then((result) =>{
-                console.log(result)
+                console.log(result.data.results)
             })
             .catch((error) =>{
                 console.warn(error)
