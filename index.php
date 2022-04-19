23c2e5bae9197147a2498148250e9bcda7ae82e6
Alenfer89 <alberioalex@gmail.com>
2022-04-19 15:50:24 +0200

    argv-tests

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
     echo 'hola';
     echo '<h1>hola</h1>';
+    $argomentoUno = $argv[1];
+    echo $argomentoUno;
+    $argomentoDue = $argv[2];
+    echo $argomentoDue;
     $italianCity = 'Milano';
     $frenchCity= 'Parigi';
     $LatinText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur aut et dolor sapiente sed ipsam quas aliquam accusantium? Nobis doloribus deleniti nam officia culpa maiores, autem id odio omnis fuga.';
