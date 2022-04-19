dcf6bde5f6cbba432b48e423e52543ddfd5288d7
Alenfer89 <alberioalex@gmail.com>
2022-04-19 16:17:15 +0200

    first-milestone-and-get-implementation

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
 <?php 
     echo 'hola';
-    echo '<h1>hola</h1>';
-    $argomentoUno = $argv[1];
-    echo $argomentoUno;
-    $argomentoDue = $argv[2];
-    echo $argomentoDue;
+    echo '<h3>hola</h3>';
+    // $argomentoUno = $argv[1];
+    // // 'word'
+    // echo $argomentoUno;
+    // $argomentoDue = $argv[2];
+    // // 'altro'
+    // echo $argomentoDue;
     $italianCity = 'Milano';
     $frenchCity= 'Parigi';
-    $LatinText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur aut et dolor sapiente sed ipsam quas aliquam accusantium? Nobis doloribus deleniti nam officia culpa maiores, autem id odio omnis fuga.';
-
-    //!html-like starting-point
+    //ù html-like starting-point
     echo '<pre>';
     echo 'Parigi'.' ';
     echo $frenchCity;
     echo '</pre>';
-    //!html-like end
+    //ù html-like end
 ?>
+<pre> <?php echo $italianCity;?> </pre>
 
-
-<pre>
+<!-- ESERCIZIO -->
 <?php 
-echo $italianCity;
+    //!ESERCIZIO
+    $LatinText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur aut et dolor sapiente sed ipsam quas aliquam accusantium? Nobis doloribus deleniti nam officia culpa maiores, autem id odio omnis fuga.';
+    $spanishInquisition = $_GET['word'];
+    echo 'La tua parola è ' . $spanishInquisition;
 ?>
-</pre>
+<h1>consegna 1</h1>
+<p>
+    <?php  
+    echo $LatinText;
+    ?>
+</p>
+<p>
+    <?php echo strlen($LatinText); ?>
+</p>
+
