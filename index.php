bb6cc4eb9e89bb457d4372149d0ea3f093458089
Alenfer89 <alberioalex@gmail.com>
2022-04-19 15:10:32 +0200

    some-echo-tests

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-
+<?php 
+    echo 'hola';
+    $italianCity = 'Milano';
+    echo '<pre>';
+    echo 'Parigi';
+    echo '</pre>';
 ?>
+
+
+<pre>
+    <?php 
+    echo $italianCity;
+    ?>
+
+</pre>
