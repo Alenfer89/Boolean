9bb4477294c6894b10502de809920201546c6ed2
Alenfer89 <alberioalex@gmail.com>
2022-04-19 15:18:57 +0200

    another-test

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php 
     echo 'hola';
+    echo '<h1>hola</h1>';
     $italianCity = 'Milano';
+    $frenchCity= 'Parigi';
+    $LatinText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur aut et dolor sapiente sed ipsam quas aliquam accusantium? Nobis doloribus deleniti nam officia culpa maiores, autem id odio omnis fuga.';
+
+    //!html-like starting-point
     echo '<pre>';
-    echo 'Parigi';
+    echo 'Parigi'.' ';
+    echo $frenchCity;
     echo '</pre>';
+    //!html-like end
 ?>
 
 
 <pre>
-    <?php 
-    echo $italianCity;
-    ?>
-
+<?php 
+echo $italianCity;
+?>
 </pre>
