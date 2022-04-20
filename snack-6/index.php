3164064e95f28e0dc7c9b1fe04f9e652c533ee3b
Alenfer89 <alberioalex@gmail.com>
2022-04-20 16:34:00 +0200

    working-background-snack-6

--- snack-6/index.php
+++ snack-6/index.php
@@ -1,6 +1,35 @@
 <?php 
     // Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
 ?>
+<?php
+
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+$bgGreen = "style='background-color:green'";
+$bgGray = "style='background-color:gray'";
+$db_keys = array_keys($db);
+$db_values = array_values($db);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,10 +41,16 @@
 </head>
 <body>
     <h1>Result</h1>
-    <pre>
-        <?php
-        
-        ?>
-    </pre>
+    <?php echo $db_keys[0] ; ?>
+    <?php for($i = 0; $i < count($db); $i++){ ; ?>
+    <div <?php if($db_keys[$i] == 'teachers'){
+        echo $bgGreen;}
+        else{
+            echo $bgGray;
+        } ?>>
+        <h3>Our <?php echo $db_keys[$i] ; ?></h3>
+
+    </div>
+    <?php } ; ?>
 </body>
 </html>
