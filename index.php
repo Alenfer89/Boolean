8e8b6bdeb397fab6519b65d972efd78042539d12
Alenfer89 <alberioalex@gmail.com>
2022-04-21 15:11:20 +0200

    first-steps

--- index.php
+++ index.php
@@ -65,9 +65,22 @@ $faqs=[
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Faqs</title>
+    <style>
+        .container{
+            margin: 0 auto;
+            width: 50%;
+            height: 100vh;
+            background-color: azure;
+        }
+    </style>
 </head>
 <body>
-    
+    <div class="container">
+        <?php foreach($faqs as $module){ ; ?>
+        <h3><?php echo $module['question'] ; ?></h3>
+        <p><?php echo $module['answer'] ; ?></p>
+        <?php } ; ?>
+    </div>
 </body>
 </html>
