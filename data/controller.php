085c4ab1bab1214c53415aa3fe78ba2fdad2de27
Alenfer89 <alberioalex@gmail.com>
2022-04-22 18:07:33 +0200

    not-working-filter

--- data/controller.php
+++ data/controller.php
@@ -2,10 +2,25 @@
     include __DIR__ . '/data.php';
 
     header('Content-Type: application/json');
-    echo json_encode(
-        [
-            'results' => $disks,
-            'length' => count($disks)
-        ]
-    );
+    if (isset($_GET['genre'])){
+        foreach($disks as $disk){
+            if($disk['genre'] == $_GET['genre']){
+                $filteredDisks[] = $disk;
+            }
+        }
+        echo json_encode(
+            [
+                'results' => $filteredDisks,
+                'length' => count($disks)
+            ]
+        );
+    }else{
+        echo json_encode(
+            [
+                'results' => $disks,
+                'length' => count($disks)
+            ]
+        );
+    }
+    
 ?>
