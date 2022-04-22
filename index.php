320e08244ca1abfc350fcbc58a078b156910c8c3
Alenfer89 <alberioalex@gmail.com>
2022-04-22 16:27:09 +0200

    css-path-error

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ include_once __DIR__ . '/data/data.php';
         <?php include __DIR__ . '/partials/header.php' ; ?>
     
     <main>
-        <h1>
+        <h1 class="text-primary">
             main 2
         </h1>
         <div>
--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,5 @@
 <?php  ; ?>
 
-<header>
-    <h1>
-        header
-    </h1>
+<header class="p-2">
+    <img class="ax-img" src="img/Spotify.png" alt="Spotify Logo">
 </header>
