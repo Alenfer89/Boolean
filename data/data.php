6979459bbfea44a358ff60b89827004b6286bb6e
Alenfer89 <alberioalex@gmail.com>
2022-04-22 16:10:57 +0200

    data-added

--- data/data.php
+++ data/data.php
@@ -1,4 +1,75 @@
 <?php 
-$disks=[];
+$disks=[
+    [
+        'title' => 'The biggest Own Way',
+        'author' => 'Miley Cyrus',
+        'genre' => 'Rock',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Social Hour ',
+        'author' => 'Jason Derulo',
+        'genre' => 'Rock',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Stairway To Groove',
+        'author' => 'Usher',
+        'genre' => 'Pop',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Blue Passion',
+        'author' => 'Elton John',
+        'genre' => 'Jazz',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Sunday feelings',
+        'author' => 'Shakira',
+        'genre' => 'Pop',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Think about snooze',
+        'author' => 'Ellie Goulding',
+        'genre' => 'Pop',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Rhythm Of symphoney',
+        'author' => 'Justin Timberlake',
+        'genre' => 'Rock',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Still need wave',
+        'author' => 'Sia',
+        'genre' => 'Rock',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Unspeakable Explosions',
+        'author' => 'Michael Jackson',
+        'genre' => 'Reggeaton',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+    [
+        'title' => 'Cha Cha Cha sounds',
+        'author' => 'Bruce Springsteen',
+        'genre' => 'Reggeaton',
+        'cover' => 'https://picsum.photos/400',
+        'year' => rand(1954 - 2022)
+    ],
+];
 $test='test';
 ?>
