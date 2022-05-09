2468437f716ae0d80b824f9959e8a730b0b88dc6
Alenfer89 <alberioalex@gmail.com>
2022-05-09 18:50:20 +0200

    comments-getters-setters of product object

--- models/Product.php
+++ models/Product.php
@@ -1,35 +1,94 @@
 <?php 
-
+/**
+ * General-main product class for this shop
+ */
 class Product {
+    /**
+     * the name of the added product
+     * @var [string] 'protected'
+     */
     protected $name;
+    /**
+     * the price of the added product
+     * @var [float] 'protected'
+     */
     protected $price;
+    /**
+     * small description of the added product
+     * @var [string]
+     */
     protected $description;
-
-    public function __construct($name, $price, $description)
+    /**
+     * boolean of the possibility to discount the given product, default FALSE
+     * @var [boolean]
+     */
+    protected $isDiscountable;
+    /**
+     * Function to add a new product to the list
+     *
+     * @param [string] $name : the given product name
+     * @param [float] $price : the given product price
+     * @param [string] $description : a small description of the product
+     * @param boolean $isDiscountable : return the ability of the item to be discounted, default FALSE
+     */
+    public function __construct($name, $price, $description, $isDiscountable = false)
     {
         $this->name = $name;
         $this->price = $price;
         $this->description = $description;
+        $this->isDiscountable = $isDiscountable;
 
     }
-
+    /**
+     * Product name changer
+     *
+     * @param [string] $name : the new name of the product
+     * @return a string with the new name of the product in the object
+     */
     public function setName($name){
-        return $this->name = $name;
+        $this->name = $name;
     }
+    /**
+     * Product price changer
+     *
+     * @param [float] $price : the new price of the product
+     * @return a number which is the new price of the product in the object
+     */
     public function setPrice($price){
-        return $this->price = $price;
+        $this->price = $price;
     }
+    /**
+     * Product description changer, keep it reasonably short
+     *
+     * @param [string] $description : the new description of the product
+     * @return a string with the new description of the product in the object
+     */
     public function setDescription($description){
-        return $this->description = $description;
+        $this->description = $description;
     }
-    public function getName($name){
-        echo $this->name;
+    /**
+     * retriever of the product name
+     *
+     * @return string
+     */
+    public function getName(){
+        return $this->name;
     }
-    public function getPrice($price){
-        echo $this->price;
+    /**
+     * retriever of the product price
+     *
+     * @return number
+     */
+    public function getPrice(){
+        return $this->price;
     }
-    public function getDescription($description){
-        echo $this->description;
+    /**
+     * retriever of the product description
+     *
+     * @return string
+     */
+    public function getDescription(){
+        return $this->description;
     }
 }
 
