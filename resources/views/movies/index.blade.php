f22e46a38a02d11e7ed39035c51e64506e44fc90
Alenfer89 <alberioalex@gmail.com>
2022-05-12 17:57:20 +0200

    change of operator for tests

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -13,7 +13,7 @@
                     <div class="col-4 p-3">
                         <div class="card border-danger" style="width: 18rem;">
                             <div class="card-body">
-                                <h5 class="card-title"> {{ $movie['title'] }} </h5>
+                                <h5 class="card-title"> {{ $movie->title }} </h5>
                                 <h6 class="card-subtitle mb-2 text-muted"> {{ $movie['original_title'] }} </h6>
                                 <p class="card-text">Country: {{ $movie['nationality'] == 'american/british' ? '\'Murica' : 'Perfida Albione' }} </p>
                                 <span href="#" class="card-link">Released: {{ $movie['date'] }} </span>
