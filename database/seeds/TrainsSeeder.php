30e9153a9b88efb2e6959170c8eedb74b16ff168
Alenfer89 <alberioalex@gmail.com>
2022-05-16 16:51:08 +0200

    popultae seeder with faker

--- database/seeds/TrainsSeeder.php
+++ database/seeds/TrainsSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
 class TrainsSeeder extends Seeder
 {
     /**
@@ -9,8 +10,20 @@ class TrainsSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker )
     {
-        //
+        for ($i=0; $i < 100 ; $i++) { 
+            $newTrain = new Train();
+            $newTrain->carrier = $faker->randomElement(['Tranitalia', 'Ytalo', 'Brecciarossa', 'Brecciargento', 'Regionalissimo']);
+            $newTrain->dep_station = $faker->city();
+            $newTrain->arr_station = $faker->city();
+            $newTrain->day = $faker->date();
+            $newTrain->departure_at = $faker->time();
+            $newTrain->arrival_at = $faker->time();
+            $newTrain->train_code = $faker->lexify('??????');
+            $newTrain->pax_carriage_nr = $faker->numberBetween(5, 10);
+            $newTrain->is_delayed = $faker->boolean();
+            $newTrain->is_canceled = $faker->boolean();
+        }
     }
 }
