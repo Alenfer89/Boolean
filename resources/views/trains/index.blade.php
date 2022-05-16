036151c50423441471af1ee10d1f5a491a3a63bb
Alenfer89 <alberioalex@gmail.com>
2022-05-16 17:22:35 +0200

    some data showing in views

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -5,6 +5,34 @@
 
 @section('main-content')
     <main>
-        <h1>content</h1>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 mb-5">
+                    <h1>Our Trains</h1>
+                </div>
+
+                @foreach ($trains as $train)
+                    <div class="col-3 mb-4 p-5">
+                        <p>Treno delle {{ substr($train->departure_at, 0, 5)  }}</p>
+                        <p>
+                            In partenza da {{ $train->dep_station }} per {{ $train->arr_station }}
+                        </p>
+                        <p>
+                            Operato da {{ $train->carrier }}
+                        </p>
+                        <p>
+                            @if ($train->is_canceled == 1)
+                                {{ 'E\' stato cancellato' }}
+                            @elseif($train->is_delayed == 1 )
+                                {{ 'Subirà un leggero ritardo' }}
+                            @elseif($train->is_delayed == 0 )
+                                {{ 'E\' in arrivo regolarmente su un binario a caso' }}
+                            @endif
+                        </p>
+                    </div>
+                @endforeach
+                
+            </div>
+        </div>
     </main>
 @endsection
