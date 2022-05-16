5b83537b1be7ee6cc8bde4eed556d7b5ff4422d4
Alenfer89 <alberioalex@gmail.com>
2022-05-16 17:00:59 +0200

    some trains seeding tests

--- database/seeds/TrainsSeeder.php
+++ database/seeds/TrainsSeeder.php
@@ -12,18 +12,22 @@ class TrainsSeeder extends Seeder
      */
     public function run(Faker $faker )
     {
-        for ($i=0; $i < 100 ; $i++) { 
+        for ($i=0; $i < 1000 ; $i++) { 
             $newTrain = new Train();
             $newTrain->carrier = $faker->randomElement(['Tranitalia', 'Ytalo', 'Brecciarossa', 'Brecciargento', 'Regionalissimo']);
             $newTrain->dep_station = $faker->city();
             $newTrain->arr_station = $faker->city();
-            $newTrain->day = $faker->date();
+            $newTrain->day = $faker->dateTimeBetween('-1 week', '+1 week');
             $newTrain->departure_at = $faker->time();
             $newTrain->arrival_at = $faker->time();
             $newTrain->train_code = $faker->lexify('??????');
             $newTrain->pax_carriage_nr = $faker->numberBetween(5, 10);
-            $newTrain->is_delayed = $faker->boolean();
             $newTrain->is_canceled = $faker->boolean();
+            if($newTrain->is_canceled == 1 ){
+                $newTrain->is_delayed = 0;
+            } else {
+                $newTrain->is_delayed = $faker->boolean();
+            }
             $newTrain->save();
         }
     }
