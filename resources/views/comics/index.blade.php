f628e20393fd701e1d97d34e21ef04dfe067161d
Alenfer89 <alberioalex@gmail.com>
2022-05-17 17:37:01 +0200

    comics-styled

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,20 +5,52 @@
 
 @section('main-content')
     <main>
-        <h1>
-            index
-        </h1>
-        @foreach ($comics as $comic)
-        <pre>
-            
-            {{$comic['title']}}
-        </pre>
-        @endforeach
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col">Series</th>
+                                <th scope="col">Type</th>
+                                <th scope="col">Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($comics as $comic)
+                                <tr>
+                                    <th scope="row">
+                                        <a href="{{route('comics.show' , $comic->id)}}">
+                                            {{ $comic->title }}
+                                        </a>
+                                    </th>
+                                    <td>
+                                        {{ $comic->series }}
+                                    </td>
+                                    <td>
+                                        {{ $comic->type }}
+                                    </td>
+                                    <td>
+                                        {{ $comic->price }}$
+                                    </td>
+                                </tr>
+                                {{-- <pre>
+                                    {{$comic['title']}}
+                                </pre> --}}
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        
+        
         
         <div>
-            <a href="{{route('comics.show' , $comic->id)}}">
+            <a href="">
                 <button>
-                    show
+                    create
                 </button>
             </a>
         </div>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,9 +5,34 @@
 
 @section('main-content')
     <main>
-        <h1>
-            show
-        </h1>
+        <div class="container-fluid px-5">
+            <div class="row px-5">
+                <div class="col-12 mb-4">
+                    <h1 class="py-4">
+                        {{ $comic->title }}
+                    </h1>
+                    <h4 class="pb-3">
+                        {{ $comic->series }}
+                    </h4>
+                </div>
+                <div class="col-4">
+                    <img class="w-100" src="{{ $comic->thumb }}" alt="{{ $comic->title }} cover image">
+                </div>
+                <div class="col-4">
+                    <p>
+                        {{ $comic->description }}
+                    </p>
+                </div>
+                <div class="col-4">
+                    <p>
+                        {{ $comic->price }}$
+                    </p>
+                    <p>
+                        {{ $comic->type }}
+                    </p>
+                </div>
+            </div>
+        </div>
         <div>
             <a href="{{route('comics.index')}}">
                 <button>
